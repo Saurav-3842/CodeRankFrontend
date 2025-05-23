5e542928e7d214e2e95afaf03e878ad730721285
Saurav <saurav0411gupta@gmail.com>
2025-05-23 13:27:28 +0530

    error set for credential page with improved middleware

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY, // expose the env variable
+  },
 };
 
 export default nextConfig;
